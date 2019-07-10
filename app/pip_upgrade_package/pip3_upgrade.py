29b002da771ede2a705e0064d4c375d440b6d660
tonyhauuk <hwtony@live.com>
2019-07-10 14:46:23 +0800

    fix pip3 update

--- app/pip_upgrade_package/pip3_upgrade.py
+++ app/pip_upgrade_package/pip3_upgrade.py
@@ -11,10 +11,3 @@ for dist in get_installed_distributions():
     print("updating:", dist.project_name, "\t")
     print(time.asctime(time.localtime(time.time())))
     call("pip3 install --upgrade " + dist.project_name, shell=True)
-
-    '''
-    call is not found in the python file
-    this is a simple demo file that function is upgrade pip python package
-    via python program beyond the code
-    '''
-
