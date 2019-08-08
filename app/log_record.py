d98ecddc0400ae869911c3564779d8ce33102665
tonyhauuk <hwtony@live.com>
2019-08-08 10:56:41 +0800

    update log file

--- app/log_record.py
+++ app/log_record.py
@@ -1,16 +1,50 @@
-import logging as LOG
+# -*- coding: utf-8 -*-
+
+import logging
+from logging import handlers
+import time, os
 
 
-def writeLog():
-    LOG.basicConfig(level=LOG.DEBUG,
-                    format='%(asctime)s %(filename)s %(threadName)s [line:%(lineno)d] %(levelname)s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename='test.log',
-                    filemode='a')
+class Logger(object):
+    levelDict = {
+        'debug': logging.DEBUG,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'error': logging.ERROR,
+        'crit': logging.CRITICAL
+    }
 
-    LOG.debug('This is a debug message')
-    LOG.info('This is a info message')
-    LOG.warning('This is a warning message')
+    def __init__(self, logName):
+        self.timestamp = str(int(time.time()))
+        self.time = time.strftime('%Y-%m-%d', time.localtime())  # time : %H:%M:%S
+        self.fileName = self.time + '-' + logName
+
+    def logger(self, level='info'):
+        # self.__checkSize__()
+
+        self.logger = logging.getLogger(self.fileName)
+        format = logging.Formatter('%(asctime)s %(filename)s %(threadName)s [line:%(lineno)d] %(levelname)s %(message)s')
+        self.logger.setLevel(self.levelDict.get(level))
+        screenPrint = logging.StreamHandler()
+        screenPrint.setFormatter(format)
+        handler = handlers.TimedRotatingFileHandler(filename=self.fileName, when='D', backupCount=3, encoding='utf-8')
+        handler.setFormatter(format)
+        self.logger.addHandler(screenPrint)
+        self.logger.addHandler(handler)
 
 
-writeLog()
+        # LOG.basicConfig(level=lev,
+        #                 format='%(asctime)s %(filename)s %(threadName)s [line:%(lineno)d] %(levelname)s %(message)s',
+        #                 datefmt='%a, %d %b %Y %H:%M:%S',
+        #                 filename=self.fileName,
+        #                 filemode='a')
+
+    def __checkSize__(self):
+        try:
+            size = os.path.getsize(self.fileName)
+            if size > 10000:
+                with open(self.fileName, 'r+') as f:
+                    f.seek(0)
+                    f.truncate()
+        except FileNotFoundError:
+            pass
