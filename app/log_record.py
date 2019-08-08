800c4a42c12708be835e2019898b39ff10730dd3
tonyhauuk <hwtony@live.com>
2019-08-08 15:33:10 +0800

    modify logger method in log_record file

--- app/log_record.py
+++ app/log_record.py
@@ -1,43 +1,48 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
+
 
 import logging
-from logging import handlers
 import time, os
 
 
-class Logger(object):
-    levelDict = {
-        'debug': logging.DEBUG,
-        'info': logging.INFO,
-        'warning': logging.WARNING,
-        'error': logging.ERROR,
-        'crit': logging.CRITICAL
-    }
 
-    def __init__(self, logName):
+
+class Logger:
+    def __init__(self):
         self.timestamp = str(int(time.time()))
         self.time = time.strftime('%Y-%m-%d', time.localtime())  # time : %H:%M:%S
-        self.fileName = self.time + '-' + logName
-
-    def logger(self, level='info'):
-        # self.__checkSize__()
-
-        self.logger = logging.getLogger(self.fileName)
-        format = logging.Formatter('%(asctime)s %(filename)s %(threadName)s [line:%(lineno)d] %(levelname)s %(message)s')
-        self.logger.setLevel(self.levelDict.get(level))
-        screenPrint = logging.StreamHandler()
-        screenPrint.setFormatter(format)
-        handler = handlers.TimedRotatingFileHandler(filename=self.fileName, when='D', backupCount=3, encoding='utf-8')
-        handler.setFormatter(format)
-        self.logger.addHandler(screenPrint)
-        self.logger.addHandler(handler)
 
 
-        # LOG.basicConfig(level=lev,
-        #                 format='%(asctime)s %(filename)s %(threadName)s [line:%(lineno)d] %(levelname)s %(message)s',
-        #                 datefmt='%a, %d %b %Y %H:%M:%S',
-        #                 filename=self.fileName,
-        #                 filemode='a')
+    def logger(self, method, message, level = 'info'):
+        levelDict = {
+            'debug': logging.DEBUG,
+            'info': logging.INFO,
+            'warning': logging.WARNING,
+            'error': logging.ERROR,
+            'crit': logging.CRITICAL
+        }
+
+
+        type = levelDict[level]
+        self.fileName = self.time + '_' + level + '.log'
+
+
+        format1 = '%(asctime)s %(filename)s %(threadName)s : [line:%(lineno)d] %(levelname)s %(message)s'
+        format2 = '%(asctime)s %(filename)s %(threadName)s : [line:%(lineno)d] %(message)s'
+
+
+        logging.basicConfig(level = type,
+                            format = format2,
+                            datefmt = '%a, %d %b %Y %H:%M:%S',
+                            filename = self.fileName,
+                            filemode = 'a')
+
+
+        logging.error(method + ' ' + message)
+
+
+        self.__checkSize__()
+
 
     def __checkSize__(self):
         try:
