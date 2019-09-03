9ce5482db5177cd0106b2720513f97f4fb26433e
tonyhauuk <hwtony@live.com>
2019-09-03 16:00:18 +0800

    update run script

--- app/crawler_state/run.py
+++ app/crawler_state/run.py
@@ -1,9 +1,41 @@
 from crawler import Crawler
 from send_email import SendEmail
+import time
+import threading
+import datetime
 
-path = './'
-c = Crawler(path)
-count = c.doJob()
-print('count: ' + str(count))
-# send = SendEmail()
-# send.getInstanse()
+
+def sendEmail():
+    start = int(time.time())
+
+    path = r''
+    receiver1 = ['xxxxxxxxxxx', 'xxxxxxxxxxxx', 'xxxxxxxxxx']
+
+    c = Crawler(path)
+    count = c.doJob()
+
+    send = SendEmail()
+    send.getInstanse(receiver1, count)
+
+    i = int(time.time()) - start
+
+    interval = 86400 - i
+    timer = threading.Timer(interval, sendEmail)
+    timer.start()
+
+
+timeStr = 14
+now = datetime.datetime.now()
+nextTime = now + datetime.timedelta(days = +1)
+nextYear = nextTime.date().year
+nextMonth = nextTime.date().month
+nextDay = nextTime.date().day
+
+nextDayTime = datetime.datetime.strptime(str(nextYear) + '-' + str(nextMonth) + '-' + str(nextDay) + ' ' + str(timeStr) + ':00:00', '%Y-%m-%d %H:%M:%S')
+timerStartTime = (nextDayTime - now).total_seconds()
+
+print(timerStartTime)
+print(nextDayTime)
+
+timer = threading.Timer(timerStartTime, sendEmail)
+timer.start()
--- app/crawler_state/send_email.py
+++ app/crawler_state/send_email.py
@@ -5,37 +5,40 @@ from email.header import Header
 from smtplib import SMTPException
 import time
 
-class SendEmail():
-    def getInstanse(self):
-        host = 'smtp.mxhichina.com'
-        user = ''               # email
-        password = ''           # password
-        port = 465
 
-        sender = ''             # sender email
-        receiver = ''           # receiver email
+class SendEmail():
+    def getInstanse(self, recv, count):
+        host = 'smtp.mxhichina.com'
+        user = ''       # sender email
+        password = ''   # password
+        port = 465  # SSL port
+
+        sender = '' # sender email
+        receiver = recv
 
         content = time.strftime("%Y-%m-%d", time.localtime())
+        text = content + ' ' + str(count) + '篇统计'
 
         msg = MIMEMultipart()
         msg['From'] = Header(sender, 'UTF-8')
-        msg['To'] = Header(receiver, 'UTF-8')
+        msg['To'] = Header(','.join(receiver), 'UTF-8')
         subject = '五矿统计'
         msg['Subject'] = Header(subject, 'UTF-8')
-        msg.attach(MIMEText(content, 'plain', 'UTF-8'))
+        msg.attach(MIMEText(text, 'plain', 'UTF-8'))
 
-        filePath = './' + content + '.xls'
+        filePath = r'/' + content + '.xls'
         attachment = MIMEText(open(filePath, 'rb').read(), 'base64', 'UTF-8')
         attachment['Content-Type'] = 'application/octet-stream'
-        attachment['Content-Disposition'] = 'attachment; filename="' + filePath +'"'
+        attachment['Content-Disposition'] = 'attachment; filename="' + content + '.xls' + '"'
         msg.attach(attachment)
 
+        client = smtplib.SMTP_SSL(host, port)
         try:
-            client = smtplib.SMTP_SSL(host, port)
             # client = smtplib.SMTP()
             # client.connect(host, port)
             client.login(user, password)
             client.sendmail(sender, receiver, msg.as_string())
-            client.quit()
         except SMTPException as e:
-           print('Exception: ' + str(e))
+            print('Exception: ' + str(e))
+        finally:
+            client.quit()
